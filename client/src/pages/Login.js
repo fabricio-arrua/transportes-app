6e855174d182eef3b8eee63458030d754b26d279
Fabricio Arrua <fabricio.arrua03@gmail.com>
2023-07-04 20:00:06 -0300

    cambios en mensaje de error

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -7,12 +7,23 @@ import Cookies from 'universal-cookie';
 const baseUrl = 'http://localhost:4000/api/empleados/logueo';
 const cookies = new Cookies();
 
+const defaultState = {
+  msgError: '',
+};
+
 class Login extends Component {
+
   state = {
     form: {
       username: '',
       password: ''
     }
+  }
+
+  constructor() {
+    super();
+    this.state = defaultState;
+    this.handleChange = this.handleChange.bind(this);
   }
 
   handleChange = async e => {
@@ -25,23 +36,42 @@ class Login extends Component {
     console.log(this.state.form);
   }
 
+  validate() {
+    let msgError = "";
+    if (!this.state.name) {
+      msgError = "El usuario o la contraseña no son correctos.";
+    }
+    if (msgError) {
+      this.setState({ msgError });
+      return false;
+    }
+    return true;
+  }
+
+  submit() {
+    if (this.validate()) {
+
+      this.setState(defaultState);
+    }
+  }
+
   iniciarSesion = async () => {
     await axios.get(baseUrl, {params: {usuario: this.state.form.username, contrasenia: this.state.form.password}})
-    .then(response => {
-      if (response.data.message != 'Datos ingresados incorrectos') {
-        cookies.set('usuario', response.data.usuario, {path: '/'});
-        cookies.set('nombre_completo', response.data.nombre_completo, {path: '/'});
+      .then(response => {
+        if (response.data.message != 'Datos ingresados incorrectos') {
+          cookies.set('usuario', response.data.usuario, {path: '/'});
+          cookies.set('nombre_completo', response.data.nombre_completo, {path: '/'});
 
-        alert(`Bienvenido ${response.data.nombre_completo}`);
+          alert(`Bienvenido ${response.data.nombre_completo}`);
 
-        window.location.href='./menu';
-      } else {
-        alert('El usuario o la contraseña no son correctos.');
-      }
-    })
-    .catch(error => {
-      console.log(error);
-    })
+          window.location.href='./menu';
+        } else {
+          this.submit();
+        }
+      })
+      .catch(error => {
+        console.log(error);
+      }) 
   }
 
   componentDidMount() { 
@@ -54,6 +84,7 @@ class Login extends Component {
     return (
       <div className='containerPrincipal'>
         <div className='containerSecundario'>
+        <span className="texto-error">{this.state.msgError}</span>
             <div className='form-group'>
               <label>Usuario: </label>
               <br />
