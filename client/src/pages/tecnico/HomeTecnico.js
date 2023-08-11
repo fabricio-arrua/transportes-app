87744954a44f4264ca145edb3d54927b09fb8558
Fabricio Arrua <fabricio.arrua03@gmail.com>
2023-08-11 20:48:16 -0300

    cambio en HomeTecnico

--- client/src/pages/tecnico/HomeTecnico.js
+++ client/src/pages/tecnico/HomeTecnico.js
@@ -1,18 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Cookies from 'universal-cookie';
 
 const cookies = new Cookies();
 
 export default class HomeTecnico extends Component {
 
-  cerrarSesion = () => {
-    cookies.remove('usuario', {path: '/'});
-    cookies.remove('nombre_completo', {path: '/'});
-    cookies.remove('tipo', {path: '/'});
-
-    window.location.href='./';
-  }
-    
   componentDidMount() { 
     if(cookies.get('tipo') !== 'T'){
       window.location.href='./';
@@ -20,15 +12,9 @@ export default class HomeTecnico extends Component {
   }
     
   render() {
-    console.log('Usuario: ' + cookies.get('usuario'));
-    console.log('Nombre completo: ' + cookies.get('nombre_completo'));
-    console.log('Tipo: ' + cookies.get('tipo'));
     return (
       <div>
-        BIENVENIDO TÉCNICO!!
-
-        <br />
-        <button onClick={()=>this.cerrarSesion()}>Cerrar sesión</button>
+        BIENVENIDO TECNICO!!
       </div>
     )
   }
