aefe7b08c4960b1e382407907d833a1b8418484a
Fabricio Arrua <fabricio.arrua03@gmail.com>
2024-01-01 20:14:59 -0300

    validaciones de chofer

--- client/src/pages/admin/actions/create/CreateChofer.js
+++ client/src/pages/admin/actions/create/CreateChofer.js
@@ -1,77 +1,220 @@
-import { Button, Form } from 'semantic-ui-react'
-import React, { useState, useEffect } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate, Link } from 'react-router-dom';
 import '../../../../css/misBtns.css'
 import Cookies from 'universal-cookie';
+import { toast, ToastContainer } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import { useFormik } from 'formik';
+import { choferValidations } from "../../../../validations/choferValidations";
 
 const cookies = new Cookies();
 
-export default function CreateChofer() {
-  const [usuario, setUsuario] = useState('');
-  const [contrasenia, setContrasenia] = useState('');
-  const [licencia, setLicencia] = useState('');
-  const [telefono, setTelefono] = useState('');
-  const [nombre, setNombre] = useState('');
+const initialValues = {
+  usuario: '',
+  contrasenia: '',
+  nombre: '',
+  licencia: '',
+  telefono: ''
+}
 
+export default function CreateChofer() {
   const navigate = useNavigate();
 
   useEffect(() => {
-    if(cookies.get('tipo') !== 'A'){
-      window.location.href='/';
+    if (cookies.get('tipo') !== 'A') {
+      window.location.href = '/';
     }
   }, [])
 
-  const postData = () => {
-		axios.post(`http://localhost:4000/api/empleados/altaChofer`, {
-			usuario,
-			contrasenia,
-			nombre,
-			licencia,
-			telefono
-		},
-    {
-      headers: {
-        Authorization: cookies.get('token'), 
+  const formik = useFormik({
+    initialValues,
+    onSubmit: values => {
+      axios.post(`http://localhost:4000/api/empleados/altaChofer`, {
+        usuario: values.usuario,
+        contrasenia: values.contrasenia,
+        nombre: values.nombre,
+        licencia: values.licencia,
+        telefono: values.telefono
       },
-    }).then(() => {
-      navigate('/abm/abmchoferes')
-    }).catch(error => {
-      console.error('Ha ocurrido un error:', error);
-    })
-  }
+        {
+          headers: {
+            Authorization: cookies.get('token'),
+          },
+        }).then((response) => {
+          if (response.data.message === 'Alta realizada con éxito') {
+            navigate('/abm/abmchoferes')
+          } else {
+            toast.error(response.data.message, {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });
+          }
+        }).catch(function (error) {
+          if (error.response) {
+            console.log(error.response.data + 'error.response.data');
+            toast.error(error.response.data, {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });
+            console.log(error.response.status + 'error.response.status');
+            toast.error('Error comuniquese con sistemas', {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });
+            console.log(error.response.header + 'error.response.header');
+            toast.error(error.response.headers, {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });
+          } else if (error.request) {
+            console.log(error.request + 'error.request');
+            toast.error(error.request, {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });
+          } else {
+            console.log(error.message + 'error.message');
+            toast.error(error.message, {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });
+          }
+          console.log(error.config + 'error.config');
+          toast.error(error.config, {
+            position: "top-center",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "colored",
+          });
+        });
+    },
+    validationSchema: choferValidations
+  })
 
   return (
     <div className="App">
       <Link to='/abm/abmchoferes' className="Btn">
         Volver
       </Link>
-      <div className="form-container">
-        <h2 className="form-title">Registro de choferes</h2>
-        <Form className="create-form">
-          <Form.Field required>
-            <label>Usuario</label>
-            <input placeholder='Usuario' onChange={(e) => setUsuario(e.target.value)}/>
-          </Form.Field>
-          <Form.Field required>
-            <label>Contraseña</label>
-            <input type='password' placeholder='Contraseña' onChange={(e) => setContrasenia(e.target.value)}/>
-          </Form.Field>
-          <Form.Field required>
-            <label>Nombre completo</label>
-            <input placeholder='Nombre completo' onChange={(e) => setNombre(e.target.value)}/>
-          </Form.Field>
-          <Form.Field required>
-            <label>Licencia</label>
-            <input placeholder='Licencia' onChange={(e) => setLicencia(e.target.value.toUpperCase())}/>
-          </Form.Field>
-          <Form.Field required>
-            <label>Telefono</label>
-            <input type='number' placeholder='Telefono' onChange={(e) => setTelefono(e.target.value)}/>
-          </Form.Field>
-          <Button className="submit-button" onClick={postData} type='submit'>Crear</Button>
-        </Form>
-      </div>
+
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="colored"
+      />
+
+      <form onSubmit={formik.handleSubmit}>
+
+        <div className='form-control'>
+          <label htmlFor='usuario'>Usuario</label>
+          <input
+            type='text'
+            name='usuario'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.usuario}>
+          </input>
+          {formik.touched.usuario && formik.errors.usuario ? <div className='error'>{formik.errors.usuario}</div> : null}
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='contrasenia'>Contraseña</label>
+          <input
+            type='password'
+            name='contrasenia'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.contrasenia}>
+          </input>
+          {formik.touched.contrasenia && formik.errors.contrasenia ? <div className='error'>{formik.errors.contrasenia}</div> : null}
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='nombre'>Nombre completo</label>
+          <input
+            type='text'
+            name='nombre'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.nombre}>
+          </input>
+          {formik.touched.nombre && formik.errors.nombre ? <div className='error'>{formik.errors.nombre}</div> : null}
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='licencia'>Licencia</label>
+          <input
+            type='text'
+            name='licencia'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.licencia}>
+          </input>
+          {formik.touched.licencia && formik.errors.licencia ? <div className='error'>{formik.errors.licencia}</div> : null}
+        </div>
+
+        <div className='form-control'>
+          <label htmlFor='telefono'>Teléfono</label>
+          <input
+            type='text'
+            name='telefono'
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
+            value={formik.values.telefono}>
+          </input>
+          {formik.touched.telefono && formik.errors.telefono ? <div className='error'>{formik.errors.telefono}</div> : null}
+        </div>
+
+        <button className='btnSubmit' type='submit'>Crear</button>
+      </form>
     </div>
   )
 }
--- client/src/validations/choferValidations.js
+++ client/src/validations/choferValidations.js
@@ -5,5 +5,5 @@ export const choferValidations = Yup.object({
     contrasenia: Yup.string().max(50, "50 caracteres máximo").required("¡Campo obligatorio!"),
     nombre: Yup.string().max(100, "100 caracteres máximo").required("¡Campo obligatorio!"),
     licencia: Yup.string().max(50, "50 caracteres máximo").required("¡Campo obligatorio!"),
-    telefono: Yup.number().matches("[0-9]{8-12}", "Formato incorrecto, ingrese entre 8 a 12 digitos").required("¡Campo obligatorio!")
+    telefono: Yup.string().matches(/^[0-9]+$/, "Solo debe ingresar numeros").min(8, "Ingrese al menos 8 dígitos").max(12, "Límite de dígitos excedido").required("¡Campo obligatorio!")
 })
